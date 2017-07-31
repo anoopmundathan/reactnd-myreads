4f394a36b2800c4f2184f6b42d56e83c5afd14c3
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-07-31 05:01:46 +0100

    Add default props to Title component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import * as BooksAPI from '../BooksAPI'
 
 import Title from './Title'
 import BookShelf from './BookShelf'
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Title = () => {
+const Title = (props) => {
     return(
         <div className="list-books-title">
-            <h1>MyReads</h1>
+            <h1>{props.name}</h1>
         </div>
     )
+}
+
+Title.defaultProps = {
+    name: 'My Reads'
+}
+
+Title.propTypes = {
+    name: PropTypes.string.isRequired
 }
 
 export default Title
