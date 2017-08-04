eb9bc3d4f57d98ca4f645611717e26aa45057e45
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-08-04 05:33:47 +0100

    Set default for select element

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,6 +10,9 @@ class Book extends Component {
     }
 
     render() {
+        
+        const { book } = this.props
+
         return(
             <div className="book">
                 <div className="book-top">
@@ -18,10 +21,12 @@ class Book extends Component {
                         style={{ 
                             width: 128, 
                             height: 193, 
-                            backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}>                        
+                            backgroundImage: `url(${book.imageLinks.thumbnail})`}}>                        
                     </div>
                     <div className="book-shelf-changer">
-                        <select onChange={this.onChange}>
+                        <select 
+                            onChange={this.onChange}
+                            value={book.shelf}>
                             <option value="none" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
@@ -30,8 +35,8 @@ class Book extends Component {
                         </select>
                     </div>
                 </div>
-                <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors[0]}</div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors[0]}</div>
             </div>
         )
     }
